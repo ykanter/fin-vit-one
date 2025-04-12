0933594bb16504975f2f3d9a116361d5826c5962
Yakov Kanter <ykanter@gmail.com>
2025-04-12 15:43:45 -0400

    chore: Remove unused logos and update dependencies with pnpm scripts

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,10 +1,6 @@
 // src/App.tsx
 
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import honoLogo from "./assets/hono.svg";
 import "./App.css";
 
 function App() {
